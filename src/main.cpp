b6f5b7d159874ea366b8ecd216b56857604f3745
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2022-08-21 18:24:23 -0300

    refactor: move headers to header file

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <time.h>
 #include <locale.h>
-
-void calculateProbabilityOfSides(int repetitions, int repeat, int counter[]);
-void handleDoor(int &door, int chosenDoor, int comparateDoor);
-void setVictoryValue(int &value, int comparateDoor, int winningDoor);
-void readsPlayMode(int &mode);
-void readsTheNumberRepetitions(int &repetitions);
-void throwTheDiceOnce(int repetitions);
-void throwTheDiceThreeTimes(int repetitions);
+#include "main.h"
 
 #define REPEAT 10000
 #define THREE_TIMES 3
